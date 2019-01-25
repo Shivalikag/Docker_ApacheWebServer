0fa82abe48100b7e307c50e6f8c2c7525923dd85
Shivalikag <shivalika888@gmail.com>
2019-01-25 17:33:15 +0530

    It has the php file

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "The Internet of Things (IoT) is a system of physical things embedded with sensors, software, electronics";
+echo " And connectivity to allow it to perform better by exchanging information with other connected devices.";
+echo " The operator or the manufacturer.";
+
+?>
+
